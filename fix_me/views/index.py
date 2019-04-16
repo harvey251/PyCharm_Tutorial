1f134101708ff54ecf2af36814b74523c23bb419
Mark Harvey <inspiredharvey@gmail.com>
2019-04-16 05:53:17 +0100

    added first view

--- fix_me/views/index.py
+++ fix_me/views/index.py
@@ -1,6 +1,6 @@
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # TODO: Ctrl+Alt+Backspace
+    return render(request, 'todos.html')
